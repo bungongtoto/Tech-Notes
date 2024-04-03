b80a994506db5d4989c3afb7abcb7d2a41510e12
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-04-03 16:18:04 +0100

    middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app  = express()
 const path = require('path')
+const {logger} = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
@@ -18,4 +31,9 @@ app.all('*', (req, res) => {
     }
 })
 
+app.use(errorHandler)
+
+
+
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+
