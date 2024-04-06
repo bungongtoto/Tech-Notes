b6b665c35808b7a5d196d43a8c426780fbc16f0c
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-04-06 14:33:15 +0100

    initial structure and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import DashLayout from "./components/DashLayout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
   return (
-    <div className="App">
+    <Routes>
       
-    </div>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<Public />} />
+
+        <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
+        </Route>{/*End Dash*/}
+
+      </Route>
+
+    </Routes>
   );
 }
 
