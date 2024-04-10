39626e651eec56f34ae2c467b6a23d1f4730bf69
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-04-10 16:31:39 +0100

    JWT Auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
 import NewNote from "./features/notes/NewNote";
 import Prefetch from "./features/auth/Prefetch";
+import PersistLogin from "./features/auth/PersistLogin";
 
 function App() {
   return (
@@ -20,23 +21,25 @@ function App() {
 
         <Route path="login" element={<Login />} />
 
-        <Route element={<Prefetch />}>
-          <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
+              <Route index element={<Welcome />} />
 
-            <Route path="users">
-              <Route index element={<UsersList />} />
-              <Route path=":id" element={<EditUser />} />
-              <Route path="new" element={<NewUserForm />} />
-            </Route>
+              <Route path="users">
+                <Route index element={<UsersList />} />
+                <Route path=":id" element={<EditUser />} />
+                <Route path="new" element={<NewUserForm />} />
+              </Route>
 
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-              <Route path=":id" element={<EditNote />} />
-              <Route path="new" element={<NewNote />} />
+              <Route path="notes">
+                <Route index element={<NotesList />} />
+                <Route path=":id" element={<EditNote />} />
+                <Route path="new" element={<NewNote />} />
+              </Route>
             </Route>
+            {/*End Dash*/}
           </Route>
-          {/*End Dash*/}
         </Route>
       </Route>
     </Routes>
--- frontend/src/features/auth/authApiSlice.js
+++ frontend/src/features/auth/authApiSlice.js
@@ -1,5 +1,6 @@
 import { apiSlice } from "../../app/api/apiSlice";
-import { logOut } from "./authSlice";
+import { logOut, setCredentials } from "./authSlice";
+
 
 export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -17,13 +18,12 @@ export const authApiSlice = apiSlice.injectEndpoints({
       }),
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
-        //   const { data } =
-           await queryFulfilled;
-        //   console.log(data)
+          const { data } = await queryFulfilled;
+          console.log(data);
           dispatch(logOut());
           setTimeout(() => {
-            dispatch(apiSlice.util.resetApiState())
-        }, 1000)
+            dispatch(apiSlice.util.resetApiState());
+          }, 1000);
         } catch (err) {
           console.log(err);
         }
@@ -31,10 +31,20 @@ export const authApiSlice = apiSlice.injectEndpoints({
     }),
     refresh: builder.mutation({
       query: () => ({
-        url: "/auth/refresh",
-        method: "GET",
+          url: '/auth/refresh',
+          method: 'GET',
       }),
-    }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+          try {
+              const { data } = await queryFulfilled
+              console.log(data)
+              const { accessToken } = data
+              dispatch(setCredentials({ accessToken }))
+          } catch (err) {
+              console.log(err)
+          }
+      }
+  }),
   }),
 });
 
